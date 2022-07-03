70da3d40749bbfb15fab27a8d85d49bb02bb6331
Yoosuf Sayyid <cena26657@gmail.com>
2022-07-03 14:15:29 +0500

    k

--- lib/constants/global_variables.dart
+++ lib/constants/global_variables.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
-String uri = 'http://https://amazon-clone-by-yoosuf.herokuapp.com/';
+// String uri = 'https://amazon-clone-by-yoosuf.herokuapp.com/';
+String uri = 'http://192.168.1.19:3000';
 
 class GlobalVariables {
   // COLORS
